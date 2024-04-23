e9828ec8d57c09e26f4eb0ab85351e1bccdfa547
Miguel Perez <migeperezjr@gmail.com>
2024-04-22 22:15:22 -0500

    finished activity 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create!(name: "Tablet", price: 399.0, description: "This is a run of the mill overpriced tablet.")
+Product.create!(name: "Mobile Phone", price: 899.0, description: "Hooray for planned obsolescence. Buy this year's model.")
+Product.create!(name: "Laptop", price: 1599.0, description: "This has features you don't want but not ones you need.")
